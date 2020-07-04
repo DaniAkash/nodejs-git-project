54d6d9f17e783cae3193673059a3fd032451763a
DaniAkash <s.daniakash@gmail.com>
2020-07-05 01:04:49 +0530

    about page created

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    console.log(res);
-    res.write("<h1>Home Page!</h1>");
-    res.end();
+    switch (request.url) {
+      case "/":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        response.write("<h1>Home Page!</h1>");
+        response.end();
+        break;
+      case "/about":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        response.write("<h2>About Page!</h2>");
+        response.end();
+        break;
+    }
   })
   .listen(8080);
